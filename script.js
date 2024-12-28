3b25ffe194ccb368032321461a03b5bf7a39f1d4
pnttrrv <ndhkptr@gmail.com>
2024-12-28 14:04:42 +0700

    "4 commit index, css, and js"

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 const toggle = document.querySelector('.menu-toggle input');
 const uul = document.querySelector('nav ul');
+const cek = document.querySelector('.check');
+
+// for menu
+const home = document.querySelector('.home');
+const fhoto = document.querySelector('.foto');
+const about = document.querySelector('.about');
+const foother = document.querySelector('.foot');
 
 toggle.addEventListener('click', function () {
   uul.classList.toggle('slide');
+});
+
+home.addEventListener('click', function () {
+  uul.classList.toggle('slide');
+  cek.checked = false;
+});
+
+fhoto.addEventListener('click', function () {
+  uul.classList.toggle('slide');
+  cek.checked = false;
+});
+
+about.addEventListener('click', function () {
+  uul.classList.toggle('slide');
+  cek.checked = false;
+});
+
+foother.addEventListener('click', function () {
+  uul.classList.toggle('slide');
+  cek.checked = false;
 });
